b4fb90aa8b9ccae6aedb75b9ca4a895b3043261a
huanling <huanling1236@gmail.com>
2024-11-22 20:28:59 +0100

    update

--- src/Pion.java
+++ src/Pion.java
@@ -1,3 +1,23 @@
 public class Pion {
+    public enum Couleur { N, B }
+
+    private Couleur couleur;
     private Case position;
+
+    public Pion(Couleur couleur) {
+        this.couleur = couleur;
+        this.position = null;
+    }
+
+    public Couleur getCouleur() {
+        return couleur;
+    }
+
+    public Case getPosition() {
+        return position;
+    }
+
+    public void setPosition(Case position) {
+        this.position = position;
+    }
 }
